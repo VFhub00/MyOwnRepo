7e296b4db09aed89d5398be42ad9f7dd4ff5a008
Vanessa Fernander <110351851+VFgit00@users.noreply.github.com>
2022-08-11 21:26:54 -0400

    New

---
+++ New.java
@@ -0,0 +1,12 @@
+public class TestingRepo {
+    public static void main(String[] args) {
+        System.out.println("TestingRepo");
+
+        for (int i = 0; i <10; i++){
+            System.out.println(i);
+        }
+    }
+}
+
+
+
