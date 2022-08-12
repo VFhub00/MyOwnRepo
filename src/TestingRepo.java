b54f6351631a2e20d218ff09e283a17dc9b92344
Vanessa Fernander <110351851+VFgit00@users.noreply.github.com>
2022-08-11 20:50:52 -0400

    Initial commit

---
+++ src/TestingRepo.java
@@ -0,0 +1,9 @@
+public class TestingRepo {
+    public static void main(String[] args) {
+        System.out.println("TestingRepo");
+
+        for (int i = 0; i <10; i++){
+            System.out.println(i);
+        }
+    }
+}
